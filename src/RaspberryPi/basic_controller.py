6c78a7864d49817362feae960418d31b7c911b9e
casterpunch <40738987+casterpunch@users.noreply.github.com>
2022-04-19 00:00:19 -0400

    Minor Bug Fixes

--- src/RaspberryPi/basic_controller.py
+++ src/RaspberryPi/basic_controller.py
@@ -1,5 +1,5 @@
 #Rasperry Pi code, swithched from arduino due to available hardware
-import RPI.GPIO
+import RPi.GPIO
 import time
 
 #Pin definitions
@@ -7,14 +7,14 @@ in1 = 24 #L298N IN1 connected to GPIO24
 in2 = 23 #L298N IN2 connected to GPIO23
 enA = 25 #L298 ENA connected to GPIO25, the ENA connects to the first of two motors connected to the L298N
 
-#RPI.GPIO Pin setup
-RPI.GPIO.setmode(RPI.GPIO.BCM)
-RPI.GPIO.setup(in1,RPI.GPIO.OUT)
-RPI.GPIO.setup(in2,RPI.GPIO.OUT)
-RPI.GPIO.setup(enA,RPI.GPIO.OUT) 
-RPI.GPIO.output(in1,RPI.GPIO.LOW)
-RPI.GPIO.output(in2,RPI.GPIO.LOW)
-power=RPI.GPIO.PWM(enA,1000) #PWM frequency of 1000Hz, later power.changeDutyCycle(x) can be used to change the speed
+#RPi.GPIO Pin setup
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+RPi.GPIO.setup(in1,RPi.GPIO.OUT)
+RPi.GPIO.setup(in2,RPi.GPIO.OUT)
+RPi.GPIO.setup(enA,RPi.GPIO.OUT) 
+RPi.GPIO.output(in1,RPi.GPIO.LOW)
+RPi.GPIO.output(in2,RPi.GPIO.LOW)
+power=RPi.GPIO.PWM(enA,1000) #PWM frequency of 1000Hz, later power.changeDutyCycle(x) can be used to change the speed
 
 power.start(50) #start the motor at 50% power
 
@@ -22,15 +22,15 @@ power.start(50) #start the motor at 50% power
 while(1):
     command = input() #read user input
     if command == "r": #run
-        RPI.GPIO.output(in1,RPI.GPIO.LOW)
-        RPI.GPIO.output(in2,RPI.GPIO.HIGH)
+        RPi.GPIO.output(in1,RPi.GPIO.LOW)
+        RPi.GPIO.output(in2,RPi.GPIO.HIGH)
         command = "."
     elif command == "q": #stop
-        RPI.GPIO.output(in1,RPI.GPIO.LOW)
-        RPI.GPIO.output(in2,RPI.GPIO.LOW)
+        RPi.GPIO.output(in1,RPi.GPIO.LOW)
+        RPi.GPIO.output(in2,RPi.GPIO.LOW)
         command = "."
     elif command == "x": #exit
-        RPI.GPIO.cleanup()
+        RPi.GPIO.cleanup()
         print("Exiting...Cleaining up GPIO")
         break
     elif command == "h": #highpower
